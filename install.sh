05a73332cf7c07b11f689e3fb44af84d273dd16a
Tyler Wilcox <tyler@wilcoxflp.com>
2023-11-28 21:50:46 -0500

    Added Kitty

--- install.sh
+++ install.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
-
+# ---------------------
 # nvim
+# ---------------------
 
 # Install package manager
 git clone --depth 1 https://github.com/wbthomason/packer.nvim\
@@ -10,27 +11,51 @@ git clone --depth 1 https://github.com/wbthomason/packer.nvim\
 
 # Link config files
 ln -s ${BASEDIR}/nvim ~/.config
-ln -s ${BASEDIR}/vim ~/.vim
 
+# ---------------------
 # tmux
+# ---------------------
 
 # Install package manager
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 # Link config file
-ln -s ${BASEDIR}/.tmux.conf ~/.tmux.conf
+ln -s ${BASEDIR}/.tmux.conf ~/.tmux.config
 
+# ---------------------
 # zsh
+# ---------------------
+
 ln -s ${BASEDIR}/.zshrc ~/.zshrc
 
-#i3
+# ---------------------
+# i3
+# ---------------------
+
 ln -s ${BASEDIR}/i3 ~/.config
 
+# ---------------------
 # i3blocks
+# ---------------------
+
 ln -s ${BASEDIR}/i3blocks ~/.config
 
+# ---------------------
 # rofi
+# ---------------------
+
 ln -s ${BASEDIR}/rofi ~/.config
 
+# ---------------------
 # alacritty
+# ---------------------
+
 ln -s ${BASEDIR}/alacritty ~/.config
+
+# ---------------------
+# kitty
+# ---------------------
+
+ln -s ${BASEDIR}/kitty ~/.config
+
+# ---------------------
